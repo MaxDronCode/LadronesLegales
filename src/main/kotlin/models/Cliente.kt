973c77b6f197cd1be1e015e19261090fed63a946
Max Dron <maxdron@icloud.com>
2024-02-18 18:35:29 +0100

    Creación Clase Cliente.kt

---
+++ src/main/kotlin/models/Cliente.kt
@@ -0,0 +1,85 @@
+package models
+
+class Cliente {
+    // Atributos
+    private var numCliente: Int = ++numClientes
+    private var dni: String = ""
+    private var nombre: String = ""
+    private var apellido1: String = ""
+    private var apellido2: String = ""
+    private var cuentas: MutableList<CuentaBancaria> = mutableListOf()
+
+
+    // Companion
+    companion object {
+        private var numClientes:Int = 1675439
+    }
+
+    // Constructores
+    constructor(){}
+    constructor(dni:String, nombre:String, apellido1:String, apellido2:String)
+
+    fun getNumCliente(): Int {
+        return this.numCliente
+    }
+    fun getDni():String {
+        return this.dni
+    }
+    fun setDni(dni:String){
+        this.dni = dni
+    }
+
+    fun getNombre():String {
+        return this.nombre
+    }
+    fun setNombre(nombre:String) {
+        this.nombre = nombre
+    }
+
+    fun getApellido1():String {
+        return this.apellido1
+    }
+    fun setApellido1(apellido:String) {
+        this.apellido1 = apellido
+    }
+
+    fun getApellido2(): String {
+        return this.apellido2
+    }
+    fun setApellido2(apellido:String) {
+        this.apellido2 = apellido
+    }
+
+    fun getTodasCuentas(): MutableList<CuentaBancaria> {
+        return this.cuentas
+    }
+
+    fun getCuentasCorrientes(): MutableList<CuentaBancaria> {
+        var cuentasCorrientes:MutableList<CuentaBancaria> = mutableListOf()
+        for (cuenta in this.cuentas) if (cuenta is CuentaCorriente) cuentasCorrientes.add(cuenta)
+        return cuentasCorrientes
+    }
+    fun setCuentaCorriente(CuentaCorriente){
+        this.cuentas.add(CuentaCorriente)
+    }
+
+    fun getCuentasAhorro(): MutableList<CuentaBancaria> {
+        var cuentasAhorro:MutableList<CuentaBancaria> = mutableListOf()
+        for (cuenta in this.cuentas) if (cuenta is CuentaAhorro) cuentasAhorro.add(cuenta)
+        return cuentasAhorro
+    }
+    fun setCuentaAhorro(CuentaAhorro) {
+        this.cuentas.add(CuentaAhorro)
+    }
+
+    override fun toString(): String {
+        return """
+            Número de Cliente: ${this.numCliente}
+            DNI:               ${this.dni}
+            Nombre:            ${this.nombre}
+            Primer Apellido:   ${this.apellido1}
+            Segundo Apellido:  ${this.apellido2}
+            Cuentas Bancárias: ${this.cuentas.size}
+        """.trimIndent()
+    }
+}
